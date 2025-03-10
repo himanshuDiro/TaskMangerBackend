1d99014f72ada86028a5cc0eab886da68eb2f2fb
himanshuDiro <himanshu@diro.io>
2025-03-10 17:19:22 +0530

    added backend port

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Server Error', error: process.env.NODE_ENV === 'development' ? err.message : {} });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
